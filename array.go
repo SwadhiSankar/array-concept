483425fa7843b9741c63a59c0d0bd21fcc3545aa
SwadhiSankar <swadhis14@gmail.com>
2024-09-05 10:27:23 +0530

    apend method

--- array.go
+++ array.go
@@ -7,7 +7,7 @@ import "fmt"
 func main() {
 	var productName [4] string
 	productName[1] = "FirstString" //assign based on index 
-	prices := [5]float64{10.2, 10.3, 10.4, 10.5}
+	prices := []float64{10.2, 10.3, 10.4, 10.5}
 	fmt.Print(prices[3])  //to access based on index
 	fmt.Print(productName)
 
@@ -21,4 +21,8 @@ func main() {
 	fmt.Print(highlightedPrices)
     fmt.Print("length : ",len(featuredPrice)) //length //2
 	fmt.Print("capacity", cap(featuredPrice)) // capacity //4
+
+	//append MEthod 
+	prices = append(prices,3.4)
+	fmt.Print(prices)
 }
