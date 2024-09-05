c00398c6dac7831fd5ee30c98af8b3d087af0d68
SwadhiSankar <swadhis14@gmail.com>
2024-09-05 10:23:15 +0530

    slicing the array

--- array.go
+++ array.go
@@ -10,4 +10,15 @@ func main() {
 	prices := [5]float64{10.2, 10.3, 10.4, 10.5}
 	fmt.Print(prices[3])  //to access based on index
 	fmt.Print(productName)
+
+	//slicing 
+	featuredPrice := prices[1:3]
+
+	//another way 
+	// prices[:3], [1:] 
+	fmt.Print(featuredPrice)
+	highlightedPrices := prices[:3]
+	fmt.Print(highlightedPrices)
+    fmt.Print("length : ",len(featuredPrice)) //length //2
+	fmt.Print("capacity", cap(featuredPrice)) // capacity //4
 }
